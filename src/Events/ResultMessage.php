4d6c2cd99d0600e2897203908462d06122e81a42
Stijn Leenknegt <stijn@diagro.be>
2022-05-06 10:01:05 +0200

    version 1.34

--- src/Events/ResultMessage.php
+++ src/Events/ResultMessage.php
@@ -1,24 +1,19 @@
 <?php
 namespace Diagro\API\Events;
 
-use Diagro\Events\BroadcastWhenOccupied;
+use Diagro\Events\CompanyUserBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Queue\SerializesModels;
 
-class ResultMessage implements ShouldBroadcast
+class ResultMessage extends CompanyUserBroadcast implements ShouldBroadcast
 {
 
-    use SerializesModels, BroadcastWhenOccupied;
+    use SerializesModels;
 
 
     public function __construct(private string $identifier, public array $data)
     {
-        $this->user_id = auth()->user()->id();
-    }
-
-    public function broadcastQueue()
-    {
-        return 'events';
+        parent::__construct();
     }
 
     public function broadcastAs(): string
